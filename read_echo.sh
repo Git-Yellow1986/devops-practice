a35b8095d0372bbe592ea09585a6239600c7ada3
matthews <cnusrinivasu@gmail.com>
2024-08-29 19:06:31 +0530

    read and echo

--- read_echo.sh
+++ read_echo.sh
@@ -3,3 +3,8 @@
 echo "Enter you name :: "
 read USERNAME
 echo "USERNAME entered is : $USERNAME"
+
+FRUITS=("APPLE" "GRAPS" "SUGARCRANE" "ORANG") # ARRAY
+echo "First fruit is : ${FRUITS[2]}"
+echo "Second fruit is : ${FRUITS[0]}"
+echo "All fruits are : ${FRUITS[@]}"
