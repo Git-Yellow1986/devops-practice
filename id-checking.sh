fe9ddf730cdd7b5901ee19512160d6f16282477b
matthews <cnusrinivasu@gmail.com>
2024-08-31 21:14:24 +0530

    finding id

--- id-checking.sh
+++ id-checking.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
     userid=$( id -u )
-    echo "user id is $userid"
-    
+    # echo "user id is $userid"
+
+    if [ $userid -ne 0 ]
+    then 
+        echo " It is not root user $userid "
+        exit 1
+        fi
