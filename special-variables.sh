11224e5e2969f0e75f723a71892c511f07032eaf
matthews <cnusrinivasu@gmail.com>
2024-08-31 11:48:49 +0530

    home directory

--- special-variables.sh
+++ special-variables.sh
@@ -3,3 +3,4 @@
 echo "@ is shows th all variables $@"
 echo "# is shows the number of variabl passed $#"
 echo "home is used to show present user's home directory $HOME"
+echo "PID of script executing id $$"
