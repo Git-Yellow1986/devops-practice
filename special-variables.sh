ab8629759d4220ce15a0d15d3d1ae365304a51c6
matthews <cnusrinivasu@gmail.com>
2024-08-31 11:55:28 +0530

    home directory

--- special-variables.sh
+++ special-variables.sh
@@ -4,4 +4,7 @@ echo "@ is shows th all variables $@"
 echo "# is shows the number of variabl passed $#"
 echo "home is used to show present user's home directory $HOME"
 echo "PID of script executing id $$"
-echo "Display present script name $0"
+echo "Display present script name(running file name) $0"
+echo "display present working directory $pwd"
+sleep 100 $
+echo "display PID of last background command $!"
