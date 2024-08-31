84e1000961939f6d52fec8e22f90d00a3c43b046
matthews <cnusrinivasu@gmail.com>
2024-08-31 12:12:00 +0530

    if condition trail

---
+++ if-condition.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+today=$present
+
+if( $today == "suday" || "monday" || "tuesday" || "wensday" || "thursday" || "frinday" || "saturday")
+echo "Please atted the class"
+else
+echo " sorry"
+fi
