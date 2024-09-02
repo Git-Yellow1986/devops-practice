42465423cf100832216fcec72049ec39ad124d64
matthews <cnusrinivasu@gmail.com>
2024-09-02 21:42:04 +0530

    this is first trails

--- practiceif.sh
+++ practiceif.sh
@@ -10,6 +10,16 @@ then
 fi
 }
 
+VALIDATE(){
+
+    if [ $1 -ne 0 ]
+    then    
+        echo "your program is not installed"
+        else
+        echo "your program is installed successfully....."
+    fi
+    }
+
 CHECK_ROOT
 
 dnf list installed mysql -y
@@ -18,7 +28,9 @@ if [ $? -ne 0 ]
 then 
     echo "Please install git, going to install it"
         dnf install mysql -y
-        
+    
+    VALIDATE
+
         else 
             echo " Mysql is already installed , nothing to do ..."
             fi
