2d039b50541da98a432ca1e401642041b1cadd25
matthews <cnusrinivasu@gmail.com>
2024-09-02 21:36:47 +0530

    this is first trails

--- practiceif.sh
+++ practiceif.sh
@@ -11,3 +11,16 @@ fi
 }
 
 CHECK_ROOT
+
+dnf list installed mysql -y
+
+if [ $? -ne 0]
+then 
+    echo "Please install git, going to install it"
+        dnf install mysql -y
+        
+        else 
+            echo " Mysql is already installed , nothing to do ..."
+            fi
+            
+    
