5e2c0b66a6070b2813423aef8ee94c2e15b83e42
matthews <cnusrinivasu@gmail.com>
2024-09-02 23:11:41 +0530

    this is first trails

--- 03-for-loop.sh
+++ 03-for-loop.sh
@@ -13,4 +13,17 @@ CHECK_ROOT
 for package in $@ # install any packege 
 do 
 dnf install $package
+    if [ $? -ne 0 ]
+    then 
+        echo " The $package is not installed, going to install it...."
+        dnf install $package -y
+        if [ $1 -ne 0 ]
+        then 
+            echo " The $2 installed is Failuere...."
+            else
+            echo " The $2 installations Success...."
+        fi
+        else
+        echo " The $package is already installed..."
+    fi
 done
