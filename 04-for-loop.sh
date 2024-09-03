bbeef098aece395f2a885bf0add8b75db5b1088c
matthews <cnusrinivasu@gmail.com>
2024-09-03 16:08:54 +0530

    Auto Mation with for loop

--- 04-for-loop.sh
+++ 04-for-loop.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 USERID=$(id -u)
+R="\e[31m "
+G="\e[31m "
+N="\e[0m "
+
+VALIDATE() {
+
+        if [ $1 -ne 0 ]
+        then 
+            echo -e "$2 is .... $R FAILED $N"
+            exit 1
+            else
+            echo -e "$2 is ....$G SUCCESS $N"
+        fi
+}
 
 CHECK_ROOT(){
 
@@ -14,5 +28,13 @@ CHECK_ROOT
 
 for PACKAGE in $@
 do 
-    echo $PACKAGE
-    done
+        dnf list installed $package
+            if [ $? -ne 0]
+                echo "$package is not intalled, going to install it"
+                    dnf install $package -y
+
+                        VALIDATE $? "Installing $package"
+             else
+                     echo "$package is already installed, nothing to do ....."
+            fi
+done
