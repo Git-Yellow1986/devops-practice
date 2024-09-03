6f0db82b1ecf9c84598e73c6e23a930465f9cffc
matthews <cnusrinivasu@gmail.com>
2024-09-03 17:36:01 +0530

    Auto Mation with for loop

--- 04-for-loop.sh
+++ 04-for-loop.sh
@@ -30,12 +30,11 @@ for package in $@
 do 
         dnf list installed $package
             if [ $? -ne 0]
+            then
                 echo "$package is not intalled, going to install it"
                     dnf install $package -y
-
                         VALIDATE $? "Installing $package"
-                        else
-                                
+               else         
                      echo "$package is already installed, nothing to do ....."
             fi
 done
