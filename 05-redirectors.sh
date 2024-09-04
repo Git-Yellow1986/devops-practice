6b93e00ce5c3432a3a7fe6d82131ff61dfafb9c8
matthews <cnusrinivasu@gmail.com>
2024-09-04 18:23:24 +0530

    add redirectors

--- 05-redirectors.sh
+++ 05-redirectors.sh
@@ -20,24 +20,24 @@ CHECK_ROOT(){
 VALIDATE(){
     if [ $1 -ne 0 ]
     then 
-        echo -e "$2 is.... $R Failure $N" | tee -a &>>$LOG_FILE
+        echo -e "$2 is.... $R Failure $N" | tee -a $LOG_FILE
     else
-        echo -e "$2 is ....$G SUCCESS $N" | tee -a &>>$LOG_FILE
+        echo -e "$2 is ....$G SUCCESS $N" | tee -a $LOG_FILE
     fi
     }
 
 
     for package in $@
     do 
-        dnf list installed $package | tee -a &>>$LOG_FILE
+        dnf list installed $package | tee -a $LOG_FILE
             if [ $? -ne 0 ]
             then 
-                echo "$package is not installed, goint ot install it " | tee -a &>>$LOG_FILE
-                dnf install $package -y | tee -a &>>$LOG_FILE
+                echo "$package is not installed, goint ot install it " | tee -a $LOG_FILE
+                dnf install $package -y | tee -a $LOG_FILE
                 
                 VALIDATE $? "Installing Package"
             else
-                echo "$package is already installed, nothing to do...." | tee -a &>>$LOG_FILE
+                echo "$package is already installed, nothing to do...." | tee -a $LOG_FILE
             fi
             done
 
