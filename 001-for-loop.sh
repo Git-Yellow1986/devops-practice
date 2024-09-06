28ebe3e3127363a3457f1183cf6181cc17032835
matthews <cnusrinivasu@gmail.com>
2024-09-06 22:50:01 +0530

    for loop

--- 001-for-loop.sh
+++ 001-for-loop.sh
@@ -18,7 +18,7 @@ CHECK_ROOT(){
     
     if [ $USERID -ne 0 ]
     then 
-        echo "Please run this script with root privelege...."
+        echo -e "$Y Please run this script with root privelege....$N"
         exit 1
     fi
 }
@@ -28,14 +28,22 @@ VALIDATE(){
    if [ $1 -ne 0 ]
    then 
     echo -e "The $2 is .....$R FAILURE $N" | tee -a $LOG_FILE
+    exit 1
     else
     echo -e "The $2 is .....$G SUCCESS $N" | tee -a $LOG_FILE
     fi
 }
+
+        USAGE(){
+            echo -e "$R USAGE:: $N sudo sh 16-redirectors.sh package1 package2...."
+        }
     echo "Script started executing at: $(date)"
 
 CHECK_ROOT
-    
+    if [ $# -eq 0 ]
+    then 
+     USAGE
+     fi
         for package in $@ # refers to all arguments passed to it
         do
          dnf list installed $package    
@@ -43,7 +51,7 @@ CHECK_ROOT
                 if [ $? -ne 0 ]
                 then 
                     echo "The $package is not installed, going to install it...." | tee -a $LOG_FILE
-                    dnf install $package -Y | tee -a $LOG_FILE
+                    dnf install $package -y | tee -a $LOG_FILE
                         VALIDATE $? "Install $package"
                     else
                         echo -e "The $package is already $Y installed, nothing to do $N" | tee -a $LOG_FILE
